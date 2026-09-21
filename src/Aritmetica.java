c9fd3e91f8f3b4dd61305fcc646031427fb2f816
Ocatvio Caballero <Octaandres07@gmail.com>
2026-09-21 19:24:26 -0300

    agregado el archivo aritmetica en POO

--- src/Aritmetica.java
+++ src/Aritmetica.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Aritmetica {
+int operando1;
+int operando2;
+
+public Aritmetica(int op1, int op2){
+    System.out.println("***Ejecutando conatructor***");
+    operando1 = op1;
+    operando2 = op2;
+}
+
+void sumar(){
+    int resutado = operando1 + operando2;
+    System.out.println("Resultado suma: " + resutado);
+}
+void resta(){
+    int resultado = operando1 - operando2;
+    System.out.println("Resultado resta: " + resultado);
+}
+
+    public static void main(String[] args) {
+        System.out.println("***Ejemplo Aritmetica***");
+        Aritmetica aritmetica1 = new Aritmetica(5, 7);
+        // aritmetica1.operando1 = 5;
+        // aritmetica1.operando2 = 7;
+        aritmetica1.sumar();
+        aritmetica1.resta();
+
+    }
 }
