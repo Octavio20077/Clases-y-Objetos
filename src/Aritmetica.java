72d166574918cfe1b3e1d3271eff000939eb6abd
Ocatvio Caballero <Octaandres07@gmail.com>
2026-09-22 15:36:34 -0300

    Agregado el archivo aritmetica

--- src/Aritmetica.java
+++ src/Aritmetica.java
@@ -2,28 +2,36 @@ public class Aritmetica {
 int operando1;
 int operando2;
 
-public Aritmetica(int op1, int op2){
-    System.out.println("***Ejecutando conatructor***");
-    operando1 = op1;
-    operando2 = op2;
+//constructor vacio
+public Aritmetica(){
+}
+
+public Aritmetica(int operando1, int operando2){
+    System.out.println("***Ejecutando constructor***");
+    this.operando1 = operando1;
+    this.operando2 = operando2;
+    System.out.println("Operador this: " + this);
 }
 
 void sumar(){
-    int resutado = operando1 + operando2;
+    int resutado = this.operando1 + this.operando2;
     System.out.println("Resultado suma: " + resutado);
 }
 void resta(){
-    int resultado = operando1 - operando2;
+    int resultado = this.operando1 - this.operando2;
     System.out.println("Resultado resta: " + resultado);
 }
 
     public static void main(String[] args) {
         System.out.println("***Ejemplo Aritmetica***");
         Aritmetica aritmetica1 = new Aritmetica(5, 7);
-        // aritmetica1.operando1 = 5;
-        // aritmetica1.operando2 = 7;
         aritmetica1.sumar();
         aritmetica1.resta();
-
+        System.out.println("dir. de memomira de obj1: " + aritmetica1);
+        System.out.println();
+        //creamos un objeto
+        Aritmetica aritmetica2 = new Aritmetica(12, 16);
+        aritmetica2.sumar();
+        System.out.println("Dir. Memoria obj2: " + aritmetica2);
     }
 }
