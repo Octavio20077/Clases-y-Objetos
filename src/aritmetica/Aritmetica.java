a66111333285d63ece00e14251611a4d8e113a88
Ocatvio Caballero <Octaandres07@gmail.com>
2026-09-23 10:36:17 -0300

    agregada la clase de encapsulaiento

--- src/aritmetica/Aritmetica.java
+++ src/aritmetica/Aritmetica.java
@@ -21,7 +21,20 @@ public Aritmetica(int operando1, int operando2){
 public void resta(){
     int resultado = this.operando1 - this.operando2;
     System.out.println("Resultado resta: " + resultado);
-}
+   }
+   public int getOperando1(){
+    return this.operando1;
+   }
 
+   public void setOperando1(int operando1){
+    this.operando1 = operando1;
+   }
+
+   public int getOperando2(){
+    return this.operando2;
+   }
+   public void setOperando2(int operando2){
+    this.operando2 = operando2;
+   }
 
 }
--- src/prueba/PruebaAritmetica.java
+++ src/prueba/PruebaAritmetica.java
@@ -6,6 +6,9 @@ public class PruebaAritmetica {
     public static void main(String[] args) {
         System.out.println("***Ejemplo aritmetica.Aritmetica***");
         Aritmetica aritmetica1 = new Aritmetica(5, 7);
+        System.out.println("Atributo operando 1: " + aritmetica1.getOperando1());
+        aritmetica1.setOperando1(5);
+        aritmetica1.setOperando2(5);
         aritmetica1.sumar();
         aritmetica1.resta();
 
