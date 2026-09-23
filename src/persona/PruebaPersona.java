e7b4854eb8396c100f6894fdfaad796717c3711e
Ocatvio Caballero <Octaandres07@gmail.com>
2026-09-23 14:51:03 -0300

    agregado el archivo persona y persona prueba

---
+++ src/persona/PruebaPersona.java
@@ -0,0 +1,37 @@
+package persona;
+
+public class PruebaPersona {
+    public static void main(String[] args) {
+        System.out.println("***Creacion de Clase y objetos Personas***");
+        Persona objeto1 = new Persona("Octavio", "Caballero");
+        objeto1.mostrarPersona();
+        //Segundo objeto
+        System.out.println();
+        Persona objeto2 = new Persona("Andres", "Ortega");
+        objeto2.mostrarPersona();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
